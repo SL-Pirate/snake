82b411deaf05fe9de68f968568b0cc0b20790faf
SL-Pirate <isiraherath626@gmail.com>
2023-01-16 00:24:55 +0530

    snake basic movement implemented

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 int main(){
     Display *display = new Display();
     draw(display);
+    Body *body = new Body(display);
     display->draw();
+    std::thread loop(mainloop, body);
+    loop.join();
 
     return 0;
 }
@@ -11,8 +14,31 @@ int main(){
 void draw(Display *display){
     //clear the literal screen
     system("clear");
+}
 
-    //The start of the snake
-    display->screen[9][29] = body;
-    display->screen[9][30] = body;
+void mainloop(Body *body){
+    //implemented for testing assuming the body is all in a single direction
+    //i.e. not bent from anywhere
+    while (true){
+        for (int i = 0; i < body->length; i++){
+            switch(body->direction){
+                case body->up:
+                    body->snake[i][body->v]++;
+                    break;
+                case body->down:
+                    body->snake[i][body->v]--;
+                    break;
+                case body->left:
+                    body->snake[i][body->h]--;
+                    break;
+                case body->right:
+                    body->snake[i][body->h]++;
+            }
+            body->addToDisplay();
+            system("clear");
+            body->display->draw();
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        }
+    }
 }
--- main.hpp
+++ main.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+#ifdef __linux__
+#include <unistd.h>
+#endif
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 const int width = 60;
 const int height = 20;
@@ -16,7 +25,28 @@ class Display{
 
     char screen[height][width];
 
-    void draw();
+    //only draws what's in the display array
+    void draw(); 
+};
+
+class Body{
+    static const int maxLength = 100;
+
+    public:
+    Display *display;
+    enum Dir{up, down, left, right};
+    //v = vertical and h = horizontal
+    enum VorH{v = 0, h = 1};
+    int length = 2;
+    //snake[unit (where 0 is the head)][cordinate (where v (as in vertical) or 0 is y and h (as in horizontal) or 1 is x)]
+    int snake[maxLength][2]; 
+    Dir direction;
+
+    Body(Display *display);
+
+    void addToDisplay();
 };
 
 void draw(Display *display);
+
+void mainloop(Body *body);
