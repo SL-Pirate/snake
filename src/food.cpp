20280a667c1adf4cfc792d8c83b24ecf2ce02de2
SL-Pirate <isiraherath626@gmail.com>
2023-04-21 20:36:43 +0530

    Improved food generation mechanism

--- src/food.cpp
+++ src/food.cpp
@@ -11,21 +11,41 @@ Food::Food(Window *gameWin, GraphicItem ***arr){
 }
 
 void Food::genFood(){
-    if(item == nullptr){
-        genCords();
-        if (arr[cords[0]][cords[1]] == nullptr){
-            item = new GraphicItem(texture, cords[0], cords[1], FOOD, this);
-            arr[cords[0]][cords[1]] = item;
-        }
-        else{
-            genFood();
-        }
-    }
-}
+    if (item == nullptr){
+        int len_emptyCords = 0;
 
-void Food::genCords(){
-    cords[0] = rand() % COLS();
-    cords[1] = rand() % ROWS();
+        for (int i = 0; i < ROWS(); i++){
+            for (int j = 0; j < COLS(); j++){
+                if (arr[i][j] == nullptr){
+                    len_emptyCords++;
+                }
+            }
+        }
+
+        int emptyCords[len_emptyCords][2];
+        int index_emptyCords = 0;
+
+        for (int i = 0; i < ROWS(); i++){
+            for (int j = 0; j < COLS(); j++){
+                if (arr[i][j] == nullptr){
+                    emptyCords[index_emptyCords][0] = i;
+                    emptyCords[index_emptyCords][1] = j;
+                    index_emptyCords++;
+
+                    if (index_emptyCords == len_emptyCords){
+                        break;
+                    }
+                }
+            }
+        }
+
+        int lucky_index = rand() % len_emptyCords;
+
+        int cords[2] = {emptyCords[lucky_index][0], emptyCords[lucky_index][1]};
+
+        item = new GraphicItem(texture, cords[0], cords[1], FOOD, this);
+        arr[cords[0]][cords[1]] = item;
+    }
 }
 
 void Food::clearItem(){
