6e23c344d767c9a7d7a50a3d0ade732706708969
SL-Pirate <isiraherath626@gmail.com>
2023-05-03 18:05:28 +0530

    changed faliure return code upon SDL_Init faliure

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char** argv){
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0){
         throw SDL_InitError();
-        return 101;
+        return -1;
     }
 
     return wxEntry(argc, argv);
